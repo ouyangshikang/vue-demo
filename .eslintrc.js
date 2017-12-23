82c130b93590b4046a5b38b343869dca434fb7ff
baituo <baituo@huanleguang.com>
2017-12-23 22:11:19 +0800

    directives 指令

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,38 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-console': 0,
-    'camelcase': 0
+    'camelcase': 0,
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    'indent': ['error', 4, { 'SwitchCase': 1 }],
+    'arrow-parens': 0,
+    'comma-dangle': 0,
+    'max-len': ['error', 140],
+    'eol-last': 0,
+    'no-param-reassign': 0,
+    'no-underscore-dangle': ['error', { "allowAfterThis": true }],
+    'arrow-body-style': 0,
+    // https://github.com/airbnb/javascript/issues/1071#issuecomment-338292067
+    'no-mixed-operators': ["error", {
+        groups: [
+          ["%", "**"],
+          ["%", "+"],
+          ["%", "-"],
+          ["%", "*"],
+          ["%", "/"],
+          ["**", "+"],
+          ["**", "-"],
+          ["**", "*"],
+          ["**", "/"],
+          ["&", "|", "^", "~", "<<", ">>", ">>>"],
+          ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
+          ["&&", "||"],
+          ["in", "instanceof"],
+        ],
+        allowSamePrecedence: false
+    }],
   }
 }
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,19 +1,54 @@
 <template>
   <div class="main">
-    <div class="directive-test">
+    <div
+        class="directive-test"
+        v-add="color"
+    >
       {{num}}
     </div>
+    <button @click="add">add</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'index',
-  data () {
-    return {
-      num: 1
+    name: 'index',
+    data () {
+        return {
+            num: 1,
+            color: '#ff0000',
+        }
+    },
+    directives: {
+        add: {
+            // 指令第一次绑定到元素时调用
+            bind (el, binding, vnode) {
+                el.style = 'color:' + binding.value
+                console.log('bind...')
+            },
+            // 被绑定元素插入父节点时调用（
+            inserted () {
+                console.log('inserted...')
+            },
+            // 元素所在的模板更新时调用
+            update () {
+                console.log('update...')
+            },
+            // 被绑定元素所在模板完成一次更新周期时调用
+            componentUpdated () {
+                console.log('componentUpdated...')
+            },
+            // 指令与元素解绑时调用
+            unbind () {
+                console.log('unbind...')
+            }
+        }
+    },
+    methods: {
+        add () {
+            this.num++
+        }
     }
-  }
 }
 </script>
 

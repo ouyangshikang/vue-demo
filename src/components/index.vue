35833ce73e93ce922cd18918176190f8029ae7dc
baituo <baituo@huanleguang.com>
2017-12-23 21:34:03 +0800

    init vue

---
+++ src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <div class="directive-test">
+      {{num}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      num: 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

d5ae7a129f4d7a835684f0e90dad67cd07cf8df0
baituo <baituo@gaoding.com>
2022-11-14 00:41:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <img
+    alt="Vue logo"
+    class="logo logo1"
+    src="@/assets/logo.svg"
+    width="125"
+    cy-data="logo1"
+  />
+  <img
+    alt="Vue logo"
+    class="logo logo2"
+    src="@/assets/logo.svg"
+    width="125"
+    data-cy="logo2"
+  />
+</template>
+
+<style scoped>
+.logo {
+  position: absolute;
+}
+.logo1 {
+  transform: translateX(100px) rotate(45deg);
+}
+.logo2 {
+  transform: rotate(45deg) translateX(100px);
+}
+</style>
